aae741df284e82954417f2098ed6a83b137e1ede
Ryan <rmurph1800@gmail.com>
2021-12-22 11:54:28 -0500

    added comments

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// display current day (and added time for reference) at the top of the page
+// set current day, exact time, and current hour
 
 const currentDay = moment().format('dddd') + ' ' + moment().format('Do MMM YYYY');
 const currentTime = moment().format('h:mm:ss a');
 const currentHour = moment().format('h');
 
+// display date and time at top of page
+
 document.getElementById('currentDay').textContent = currentDay;
 document.getElementById('currentTime').textContent = currentTime;
+
+// set text and hour sent to local storage 
 
 $('.saveBtn').on('click', function() {
     console.log(this);
@@ -15,9 +19,7 @@ $('.saveBtn').on('click', function() {
     localStorage.setItem(time, text);
 });
 
-for (let i =8;i <21;i++) {
-    $(`#hour${i} .description`).val(localStorage.getItem(`hour${i}`));
-}
+// checks to see what the number split off at the hour will be, then if that is less than equal to or greater than the current hour defined above will change the color of the block accordingly
 
 function colorChange() {
     $('.time-block').each(function() {
@@ -41,3 +43,9 @@ function colorChange() {
 }
 
 colorChange();
+
+// loop over all 13 hour gets based off of changing number
+
+for (let i =8;i <21;i++) {
+    $(`#hour${i} .description`).val(localStorage.getItem(`hour${i}`));
+}
